5a258615d9d878fb8c9bffee0f699e7b59545e58
gorgonun <polariusvulcano@gmail.com>
2019-10-25 16:52:52 -0300

    Added complete catch

--- src/main/scala/CatalogCrawler.scala
+++ src/main/scala/CatalogCrawler.scala
@@ -7,13 +7,61 @@ object CatalogCrawler {
 
   val url = "https://classificados.inf.ufsc.br/latestads.php"
 
-  case class Item(category: String, title: String, link: String, image: String) {
+  case class CompleteInfo(description: String, seller: String, email: String, street: String, neighborhood: String, city: String, gender: String, price: String, contract: Boolean, basicsExpenses: Boolean, laundry: Boolean, internet: Boolean, animals: Boolean, expiration: String, postDate: String)
+
+  case class Item(category: String, title: String, link: String, image: String, completeInfo: Option[CompleteInfo] = None) {
+
     def completeUrl: String = "https://classificados.inf.ufsc.br/" + link
+
+//    def getCompleteInfo: CompleteInfo = {
+//      completeInfo match {
+//        case Some(r) => r
+//        case _ => catchCompleteInfo
+//      }
+//    }
+
+
+    def catchCompleteInfo = {
+      val doc = Jsoup.connect(completeUrl).get()
+      val data = doc
+        .selectFirst("table tr td form table tbody")
+        .select("tbody")
+        .select("tr")
+
+      val attributeList = data.iterator.asScala.map(row => row.select("td")).map(x => x.text).toList//.filter(str => str.size != 2).toMap
+      attributeList.foreach(x => println(x))
+      //        .select("tr")
+
+//
+//      val attributes = data
+//          .map(infos => )
+
+//      data
+//        .map(infos => Try(CompleteInfo(
+//          description = infos.get(17).text,
+//          seller = infos.get(20).text,
+//          email = infos.get(22).text,
+//          price = infos.get(24).text,
+//          expiration = infos.get(30).text,
+//          postDate = infos.get(5).text,
+//          infos.get(6).text,
+//          infos.get(7).text,
+//          infos.get(8).text,
+//          infos.get(1).text,
+//          infos.get(1).text,
+//          infos.get(1).text,
+//          infos.get(1).text,
+//          infos.get(1).text,
+//
+//        )))
+//      data.foreach(d => println(d))
+    }
   }
 
-  def main(args: Array[String]): Unit = {
 
+  def main(args: Array[String]): Unit = {
     val doc = Jsoup.connect(url).get()
+
     val data = doc
       .select("table[class=box]")
       .get(0)
@@ -32,6 +80,9 @@ object CatalogCrawler {
         case Success(r) => Some(r)
         case Failure(_) => None
       }
+      .toList
+
+    items.head.catchCompleteInfo
   }
 
 }
